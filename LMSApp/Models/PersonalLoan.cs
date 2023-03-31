5216bdd22d76d8bf4fac5296ecc336a865af54cd
kylopandanan <jhanseptkylopandanan@gmail.com>
2023-03-31 22:49:19 +0800

    PersonalLoan

--- LMSApp/Models/PersonalLoan.cs
+++ LMSApp/Models/PersonalLoan.cs
@@ -2,5 +2,26 @@
 {
     public class PersonalLoan
     {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public int MinLoanAmount { get; set; }
+        public int MaxLoanAmount { get; set;}
+        public int MinPaymentTerm { get; set;}
+        public int MaxPaymentTerm { get; set;}
+
+        public PersonalLoan() 
+        {
+        
+        }
+
+        public PersonalLoan(int id, string description, int minLoanAmount, int maxLoanAmount, int minPaymentTerm, int maxPaymentTerm)
+        {
+            Id = id;
+            Description = description;
+            MinLoanAmount = minLoanAmount;
+            MaxLoanAmount = maxLoanAmount;
+            MinPaymentTerm = minPaymentTerm;
+            MaxPaymentTerm = maxPaymentTerm;
+        }
     }
 }
