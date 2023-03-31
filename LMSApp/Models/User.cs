2f17bb0f60a6b03338a587fe86039bc89b38260a
kylopandanan <jhanseptkylopandanan@gmail.com>
2023-03-31 22:32:56 +0800

    Add project files.

---
+++ LMSApp/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace LMSApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly Birthdate { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+    
+
+}
