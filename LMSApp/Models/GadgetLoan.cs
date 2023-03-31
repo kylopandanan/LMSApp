c0c5daa57893a09fd6b6c2f818e9b161fd369a09
kylopandanan <jhanseptkylopandanan@gmail.com>
2023-04-01 00:43:36 +0800

    gadget Attri

--- LMSApp/Models/GadgetLoan.cs
+++ LMSApp/Models/GadgetLoan.cs
@@ -2,5 +2,26 @@
 {
     public class GadgetLoan
     {
+        public int Id { get; set; }
+        public string? GadgetName { get; set; }
+        public string? Description { get; set;}
+        public int Price { get; set; }
+        public float ThreeMonInt{ get; set; }     
+        public float SixMonInt { get; set; }
+        public float TwelveMonInt { get; set; }
+
+        public GadgetLoan() 
+        { }
+
+        public GadgetLoan(int id, string? gadgetName, string? description, int price, float threeMonInt, float sixMonInt, float twelveMonInt)
+        {
+            Id = id;
+            GadgetName = gadgetName;
+            Description = description;
+            Price = price;
+            ThreeMonInt = threeMonInt;
+            SixMonInt = sixMonInt;
+            TwelveMonInt = twelveMonInt;
+        }
     }
 }
